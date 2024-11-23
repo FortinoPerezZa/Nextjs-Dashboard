e1f67a7bf80e34e4aa60316c6b706d641b02d3b5
FortinoPerezZa <fortinoperezzarate@gmail.com>
2024-11-22 21:41:23 -0600

    Primer commit: subir proyecto Next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
+import '@/app/ui/global.css';
+import SideNav from './ui/dashboard/sidenav';
+import { ReactNode } from 'react';
+
+export default function Layout(
+  {children} : {children: ReactNode})
+  {
+    return (
+      <html>
+        <body>
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+    </body>
     </html>
   );
-}
+  }
